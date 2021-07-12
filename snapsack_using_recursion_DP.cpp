e698ddcd1a98a3a4d8c38d1d211836388e5aa091
Deepanshu Kushwaha <56316738+Deep2631@users.noreply.github.com>
2021-07-12 11:48:40 +0530

    snapsack_using_recursion_DP

---
+++ snapsack_using_recursion_DP.cpp
@@ -0,0 +1,42 @@
+///////////////   DYNAMIC PROGRAMMING ////////////
+////  KNAPSACK   USING RECUSION ////////
+///////   FIND MAX PROFIT  /////////
+#include<bits/stdc++.h>
+using namespace std;
+int knapsack(int w[] , int v[] , int W , int n){
+	if (n== 0 || W==0)
+	{
+		return 0;
+
+	}
+	if (w[n-1] <= W)
+	{
+		return v[n-1] + knapsack(w , v, (W-w[n-1]) , (n-1));
+	}else{
+		return knapsack(w,v,W,n-1);
+	}
+	if (w[n-1]>W)
+	{
+			return knapsack(w,v,W,n-1);
+	}
+
+
+}
+int main(){
+int n;
+cin>>n;
+int w[n];
+int v[n];
+int W;
+for (int i = 0; i < n; ++i)
+	{
+		cin>>w[i];
+	}	
+	for (int i = 0; i < n; ++i)
+	{
+	cin>>v[i];
+	}
+	cin>>W;
+	cout<< knapsack(w , v, W , n)<<endl;
+
+}
